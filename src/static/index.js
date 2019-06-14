c29a748f02539fe738b1f6be6f5189f1290a56f4
seon <seonu0823@gmail.com>
2019-06-14 23:09:42 +0900

    #2-3 Chat with SocketIO

--- src/static/index.js
+++ src/static/index.js
@@ -1,7 +1,18 @@
 const socket = io('/');
 
-socket.on('hello', () => console.log('Somebody said hello'));
+function sendMessage(message) {
+    socket.emit('newMessage', { message });
+    console.log(`You : ${ message }`);
+};
 
-socket.emit('helloGuys');
+function setNickname(nickname) {
+    socket.emit('setNickname', { nickname });
+};
 
-setTimeout(() => socket.emit('helloGuys'), 4000);
+function handleMessageNotif(data) {
+    const { message, nickname } = data;
+
+    console.log(`${ nickname } : ${ message }`);
+};
+
+socket.on('messageNotif', handleMessageNotif);
