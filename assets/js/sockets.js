84b1ca5abacea80e9875af1f9b01c20427e15c2a
seon <seonu0823@gmail.com>
2019-06-26 23:30:41 +0900

    #5-0 Refactoring

---
+++ assets/js/sockets.js
@@ -0,0 +1,15 @@
+import { handleNewUser } from "./notifications";
+
+let socket = null;
+
+export const getSocket = () => socket;
+
+export const updateSocket = aSocket => (socket = aSocket);
+
+export const initSockets = aSocket => {
+    const { events } = window;
+
+    updateSocket(aSocket);
+
+    aSocket.on(events.newUser, handleNewUser);
+};
