6f1c82f54d5c4bc2d015ed8d44f705a096a8621f
seon <seonu0823@gmail.com>
2019-07-01 22:22:20 +0900

    #5-2 Chat part One

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,12 +1,13 @@
-const notifications = document.getElementById('jsNotifications');
+const body = document.querySelector('body');
 
 const fireNotification = (text, color) => {
     const notification = document.createElement('div');
 
     notification.innerText = text;
     notification.style.backgroundColor = color;
+    notification.className = 'notification';
 
-    notifications.appendChild(notification);
+    body.appendChild(notification);
 };
 
 export const handleNewUser = ({ nickname }) =>
